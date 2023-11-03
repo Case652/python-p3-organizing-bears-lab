ef8858efd24ea22169c81ce2f76369bccdd6941d
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-11-03 08:28:29 -0500

    bears planted

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES 
+("Mr. Chocolate",1,"m","Brown","?",1),
+("Rowdy",1,"m","Brown","?",1),
+("Tabitha",1,"m","Brown","?",1),
+("Sergeant Brown",1,"m","Brown","?",1),
+("Melissa",1,"m","Brown","?",1),
+("Grinch",1,"m","Brown","?",1),
+("Wendy",1,"m","Brown","?",1),
+(null,15,"m","Brown","?",0)
